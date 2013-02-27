831b94b2d301dfee67f00e0c41c94e41a0b94030
slamet <slametnhd@gmail.com>
2013-02-27 04:53:46 -0800

    page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < ApplicationController
   def index
   	#@products = Product.order(:title)
   	#kaminari
-  	@products = Product.page(params[:page]).per(8)
+  	@products = Product.page(params[:page]).per(9)
     #will_paginate
     #@products = Product.paginate(:page => params[:page])
 
